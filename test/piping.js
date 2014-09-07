b4e4cb1881e3212651b0a860d5f60070a196f002
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-09-07 16:27:52 +0600

    Add tests on piping assertions

---
+++ test/piping.js
@@ -0,0 +1,25 @@
+/* global describe, it */
+
+var array = require('stream-array');
+var assert = require('../index.js');
+var should = require('should');
+describe('assert piping', function () {
+	it('should fail with first assertion', function (done) {
+		array([1, 2])
+			.pipe(assert.length(1))
+			.pipe(assert.length(2))
+			.on('end', function (err) {
+				should.exist(err);
+				done();
+			});
+	});
+	it('should fail with second assertion', function (done) {
+		array([1, 2])
+			.pipe(assert.length(2))
+			.pipe(assert.length(1))
+			.on('end', function (err) {
+				should.exist(err);
+				done();
+			});
+	});
+});
