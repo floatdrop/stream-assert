64d6ed14156e5c36e781bd7d08c2360c0395803d
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-09-15 04:02:21 +0600

    Refreshed piping tests

--- test/piping.js
+++ test/piping.js
@@ -10,16 +10,18 @@ describe('assert piping', function () {
 			.pipe(assert.length(2))
 			.on('end', function (err) {
 				should.exist(err);
+				err.message.should.eql('Stream length 2 not equal 1');
 				done();
 			});
 	});
-	
+
 	it('should fail with second assertion', function (done) {
 		array([1, 2])
 			.pipe(assert.length(2))
 			.pipe(assert.length(1))
 			.on('end', function (err) {
 				should.exist(err);
+				err.message.should.eql('Stream length 2 not equal 1');
 				done();
 			});
 	});
