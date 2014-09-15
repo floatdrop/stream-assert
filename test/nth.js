2f5aa6bedca8472f3b05942e381343c2b1a37d6d
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-09-15 12:20:21 +0600

    Update nth tests

--- test/nth.js
+++ test/nth.js
@@ -6,24 +6,30 @@ var should = require('should');
 var to = require('tobe');
 
 describe('assert.nth', function () {
-	it('should check first object', function (done) {
+	it('should check nth object', function (done) {
 		array([1])
 			.pipe(assert.nth(0, to.be.eql(1)))
 			.on('end', done);
 	});
 
-	it('should check second object', function (done) {
+	it('should emit error on wrong nth object', function (done) {
 		array([1, 2])
-			.pipe(assert.nth(1, to.be.eql(2)))
-			.on('end', done);
-	});
-
-	it('should emit end with error on wrong assertion', function (done) {
-		array([1])
-			.pipe(assert.first(to.be.eql(2)))
+			.pipe(assert.nth(0, to.be.eql(2)))
 			.on('end', function (err) {
 				should.exist(err);
 				done();
 			});
+	});
+
+	it('should have first shortcut', function (done) {
+		array([1])
+			.pipe(assert.first(to.be.eql(1)))
+			.on('end', done);
+	});
+
+	it('should have second shortcut', function (done) {
+		array([1, 2])
+			.pipe(assert.second(to.be.eql(2)))
+			.on('end', done);
 	});
 });
